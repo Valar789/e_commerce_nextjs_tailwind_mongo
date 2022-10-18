6f35db83365b0c3e303cf04e801063222ed40b58
Valar789 <luismzo789@gmail.com>
2022-10-18 08:19:22 -0500

    add eslint add layout

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + -"Ecommerce" : "Ecommerce"}</title>
+        <meta name="description" content="Ecommerce" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col min-h-screen  min-w-full justify-between">
+        <header className="">
+          <nav className="flex h-12 justify-between items-center px-4 shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">Ecommerce</a>
+            </Link>
+            <div>
+                <Link href=''><a className="p-2">Cart</a></Link>
+                <Link href=''><a className="p-2">Login</a></Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex justify-center items-center h-10 shadow-inner">Copyright @2022 <Link href='https://localhostweb.vercel.app/'><a>LocalHost</a></Link></footer>
+      </div>
+    </>
+  );
+}
